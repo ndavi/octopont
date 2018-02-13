028dd05d2d0de07ccd155a35625f34cf846af16a
ndavi <nico01.david@gmail.com>
2018-02-13 23:02:14 +0100

    Working send video color

--- converter/ArtNetToOscConverter.py
+++ converter/ArtNetToOscConverter.py
@@ -14,20 +14,26 @@ class ArtNetToOSCConverter(object):
         self.readConfig()
         self.firstMessage = "/color/light/1"
         self.secondMessage = "/color/light/2"
+        self.frameToSendFirstMessage = [0,0,0]
+        self.frameToSendSecondMessage = [0,0,0]
 
     def readConfig(self):
         self.config.read('config.cfg')
 
     def convert(self, artnetArray):
-        firstMessageValues = [
-            artnetArray.framedata[0],
-            artnetArray.framedata[1],
-            artnetArray.framedata[2]
-        ]
-        secondMessageValues = [
-            artnetArray.framedata[3],
-            artnetArray.framedata[4],
-            artnetArray.framedata[5]
-        ]
-        self.osc.sendDefault(self.firstMessage, "VIDEO", firstMessageValues, firstMessageValues)
-        self.osc.sendDefault(self.secondMessage, "VIDEO", secondMessageValues, secondMessageValues)
+        if artnetArray.framedata[0] != 0:
+            self.frameToSendFirstMessage[0] = artnetArray.framedata[0] / 255.0
+        if artnetArray.framedata[1] != 0:
+            self.frameToSendFirstMessage[1] = artnetArray.framedata[1] / 255.0
+        if artnetArray.framedata[2] != 0:
+            self.frameToSendFirstMessage[2] = artnetArray.framedata[2] / 255.0
+
+        if artnetArray.framedata[3] != 0:
+            self.frameToSendSecondMessage[0] = artnetArray.framedata[3] / 255.0
+        if artnetArray.framedata[4] != 0:
+            self.frameToSendSecondMessage[1] = artnetArray.framedata[4] / 255.0
+        if artnetArray.framedata[5] != 0:
+            self.frameToSendSecondMessage[2] = artnetArray.framedata[5] / 255.0
+
+        self.osc.sendDefault(self.firstMessage, "VIDEO", self.frameToSendFirstMessage, self.frameToSendFirstMessage)
+        self.osc.sendDefault(self.secondMessage, "VIDEO", self.frameToSendSecondMessage, self.frameToSendSecondMessage)
